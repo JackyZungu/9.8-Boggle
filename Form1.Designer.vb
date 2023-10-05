4bdc1541e70c1bccbd2c82e5d41910dbb96921d6
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-05 06:25:30 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        btnShake = New Button()
+        Label21 = New Label()
+        Label22 = New Label()
+        Label23 = New Label()
+        Label24 = New Label()
+        Label25 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(59, 61)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(141, 61)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(231, 61)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Label3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(342, 61)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(53, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Label4"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(449, 63)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Label5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(59, 99)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(53, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Label6"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(141, 99)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(53, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Label7"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(231, 99)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(53, 20)
+        Label8.TabIndex = 7
+        Label8.Text = "Label8"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(342, 100)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(53, 20)
+        Label9.TabIndex = 8
+        Label9.Text = "Label9"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(449, 100)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(61, 20)
+        Label10.TabIndex = 9
+        Label10.Text = "Label10"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(59, 144)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(61, 20)
+        Label11.TabIndex = 10
+        Label11.Text = "Label11"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(141, 144)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(61, 20)
+        Label12.TabIndex = 11
+        Label12.Text = "Label12"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(231, 144)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(61, 20)
+        Label13.TabIndex = 12
+        Label13.Text = "Label13"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(342, 144)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(61, 20)
+        Label14.TabIndex = 13
+        Label14.Text = "Label14"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(449, 144)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(61, 20)
+        Label15.TabIndex = 14
+        Label15.Text = "Label15"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(59, 188)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(61, 20)
+        Label16.TabIndex = 15
+        Label16.Text = "Label16"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Location = New Point(141, 188)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(61, 20)
+        Label17.TabIndex = 16
+        Label17.Text = "Label17"
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Location = New Point(231, 188)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(61, 20)
+        Label18.TabIndex = 17
+        Label18.Text = "Label18"
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Location = New Point(342, 188)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(61, 20)
+        Label19.TabIndex = 18
+        Label19.Text = "Label19"
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Location = New Point(449, 188)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(61, 20)
+        Label20.TabIndex = 19
+        Label20.Text = "Label20"
+        ' 
+        ' btnShake
+        ' 
+        btnShake.Location = New Point(198, 324)
+        btnShake.Name = "btnShake"
+        btnShake.Size = New Size(94, 55)
+        btnShake.TabIndex = 20
+        btnShake.Text = "Reshuffle"
+        btnShake.UseVisualStyleBackColor = True
+        ' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Location = New Point(59, 221)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(61, 20)
+        Label21.TabIndex = 21
+        Label21.Text = "Label21"
+        ' 
+        ' Label22
+        ' 
+        Label22.AutoSize = True
+        Label22.Location = New Point(141, 221)
+        Label22.Name = "Label22"
+        Label22.Size = New Size(61, 20)
+        Label22.TabIndex = 22
+        Label22.Text = "Label22"
+        ' 
+        ' Label23
+        ' 
+        Label23.AutoSize = True
+        Label23.Location = New Point(231, 221)
+        Label23.Name = "Label23"
+        Label23.Size = New Size(61, 20)
+        Label23.TabIndex = 23
+        Label23.Text = "Label23"
+        ' 
+        ' Label24
+        ' 
+        Label24.AutoSize = True
+        Label24.Location = New Point(342, 223)
+        Label24.Name = "Label24"
+        Label24.Size = New Size(61, 20)
+        Label24.TabIndex = 24
+        Label24.Text = "Label24"
+        ' 
+        ' Label25
+        ' 
+        Label25.AutoSize = True
+        Label25.Location = New Point(449, 223)
+        Label25.Name = "Label25"
+        Label25.Size = New Size(61, 20)
+        Label25.TabIndex = 25
+        Label25.Text = "Label25"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label25)
+        Controls.Add(Label24)
+        Controls.Add(Label23)
+        Controls.Add(Label22)
+        Controls.Add(Label21)
+        Controls.Add(btnShake)
+        Controls.Add(Label20)
+        Controls.Add(Label19)
+        Controls.Add(Label18)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "  Boggle"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents btnShake As Button
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnShake_Click(sender As Object, e As EventArgs) Handles btnShake.Click
+        Dim i As Integer
+        Dim n As Integer
+        For i = 1 To 25
+            n = Int(26 * Rnd()) + 65
+
+            'Declare an array for the Labels
+            Dim FoundControls As Control() = Me.Controls.Find("Label" & i, True)  'Find the label with the name "Label#" - where # is the current index
+
+            If FoundControls.Count > 0 Then
+
+                FoundControls(0).Text = Chr(n) 'Change its text
+            End If
+        Next
+    End Sub
+End Class
